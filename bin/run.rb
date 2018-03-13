362b34c9952c6cd3a1ebf48ab365700ee677511e
victoria-huang <vctrah@gmail.com>
2018-03-13 15:40:33 -0400

    added new methods to print movies in order

--- bin/run.rb
+++ bin/run.rb
@@ -5,8 +5,4 @@ require_relative "../lib/command_line_interface.rb"
 
 welcome
 character = get_character_from_user
-# show_character_movies(character)
-
-
-character_hash = get_character_movies_from_api(character)
-find_films(character_hash, character)
+show_character_movies(character)

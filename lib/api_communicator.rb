cc8627124b4b0417bb0e62b9094f78ba6fc3fdcd
adamtowers <hello@adamtowers.design>
2018-03-13 14:55:54 -0400

    split the method bonus

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,24 +2,21 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character)
+def get_characters_from_api
   #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
+  #films_collection = []
 
-  films_collection = []
-
-  character_hash["results"].each do |star_wars_characters|
-    if character == star_wars_characters["name"].downcase
-      star_wars_characters["films"].each do |url|
-        film_info = RestClient.get(url)
-        film_hash = JSON.parse(film_info)
-        films_collection << film_hash
-
-      end
-    end
-  end
-
+  character_hash["results"]#.each do |star_wars_characters|
+  #   if character == star_wars_characters["name"].downcase
+  #     star_wars_characters["films"].each do |url|
+  #       film_info = RestClient.get(url)
+  #       film_hash = JSON.parse(film_info)
+  #       films_collection << film_hash
+  #     end
+  #   end
+  # end
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -29,6 +26,22 @@ def get_character_movies_from_api(character)
   # this collection will be the argument given to `parse_character_movies`
   #  and that method will do some nice presentation stuff: puts out a list
   #  of movies by title. play around with puts out other info about a given film.
+  # films_collection
+end
+
+def get_character_movies(character)
+  films_collection = []
+
+  get_characters_from_api.each do |star_wars_characters|
+    if character == star_wars_characters["name"].downcase
+      star_wars_characters["films"].each do |url|
+        film_info = RestClient.get(url)
+        film_hash = JSON.parse(film_info)
+        films_collection << film_hash
+      end
+    end
+  end
+
   films_collection
 end
 
@@ -45,12 +58,12 @@ def parse_character_movies(films_hash)
 end
 
 def show_character_movies(character)
-  films_hash = get_character_movies_from_api(character)
+  get_characters_from_api
+  films_hash = get_character_movies(character)
   parse_character_movies(films_hash)
 end
 
 ## BONUS
-
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?

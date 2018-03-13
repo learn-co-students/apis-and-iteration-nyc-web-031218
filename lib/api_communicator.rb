1236866e46f6cfa8d8946b8cd27e5223ac44c43a
github username <github email address>
2018-03-13 11:52:03 -0400

    first switch-off

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,9 +6,13 @@ def get_character_movies_from_api(character)
   #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  
+  #binding.pry
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
+  character_hash["results"].select do |character_index|
+    character_index["name"] == character
+    
+  end
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
   # return value of this method should be collection of info about each film.
@@ -26,7 +30,7 @@ def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
   parse_character_movies(films_hash)
 end
-
+ get_character_movies_from_api("Luke Skywalker")
 ## BONUS
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?

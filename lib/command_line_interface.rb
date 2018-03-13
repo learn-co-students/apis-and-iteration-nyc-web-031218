8eece3d9cc48c853ab7e5dab1596fac853af2bce
github username <github email address>
2018-03-13 15:17:02 -0400

    Solved the lab

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,19 @@
 def welcome
   # puts out a welcome message here!
+  puts "Welcome!"
 end
 
 def get_character_from_user
-  puts "please enter a character"
+  puts "Please enter a character"
+  name = gets.chomp.downcase
+  name = name.split(" ").map {|word| word.capitalize}.join(" ")
+  if name_hash.include?(name)
+    name
+  else
+    get_character_from_user
+  end
+  # until(name_hash.include?(name))
+  #   get_character_from_user
+  # end
   # use gets to capture the user's input. This method should return that input, downcased.
 end

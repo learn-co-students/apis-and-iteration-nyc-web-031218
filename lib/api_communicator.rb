2b2d2e43d07809944c99f0d01e5372059a4c1142
harim1206 <harim1206@gmail.com>
2018-03-13 15:20:50 -0400

    complete parse_character_movies function

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -14,6 +14,7 @@ def get_character_movies_from_api(character)
 
   character_object = character_hash["results"].find{|object|
 
+
     object["name"].downcase == character
   } #array
 
@@ -36,6 +37,7 @@ def get_character_movies_from_api(character)
 
 
 
+
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -47,13 +49,45 @@ def get_character_movies_from_api(character)
   #  of movies by title. play around with puts out other info about a given film.
 end
 
-def parse_character_movies(films_hash)
+def parse_character_movies(films_arr)
   # some iteration magic and puts out the movies in a nice list
+  # {"title"=>"The Empire Strikes Back",
+  # "episode_id"=>5,
+  # "director"=>"Irvin Kershner",
+  # "producer"=>"Gary Kurtz, Rick McCallum",
+  # "release_date"=>"1980-05-17"}
+
+
+  #
+  # Title: "The Empire Strikes Back"
+  # Episode ID:
+  # Director:
+  # Producer:
+
+  # Title:
+  # Episode ID:
+  # ..
+
+  parsed_result = ""
+
+  films_arr.each do |film|
+    film.keys.each do |key|
+
+      parsed_result += "#{key}: #{film[key]} \n"
+    end
+    
+    parsed_result += "\n"
+  end
+
+  puts parsed_result
+
+
 end
 
 def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
   parse_character_movies(films_hash)
+  # binding.pry
 end
 
 ## BONUS
